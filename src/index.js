ac58a49ee5c7b7df9632c9dcae528b81304dfd85
git config --global user.name <aibolit999999999@gmail.com>
2022-01-29 00:36:31 +0300

    fix: modified index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix){return[]};
+  return matrix.reduce((git, towel, index) => {
+    if (index % 2 == 0) git.push(...towel)
+    else git.push(...towel.reverse())
+    return git
+  }, [])
 }
